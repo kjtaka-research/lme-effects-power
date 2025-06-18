e07547008fc36fc96e57d8d862faeedef542aa6d
kjtaka-research <kjtakaresearch@gmail.com>
2025-06-18 09:16:23 -0400

    Update lmerd_function_6-17-25.R
    Streamlining code

--- lmerd_function_6-17-25.R
+++ lmerd_function_6-17-25.R
@@ -11,32 +11,34 @@
 
 lmer.d = function (model = model, contrast = c(-0.5, 0.5), formatted = F) {
   require(lme4)
+  require(dplyr)
+  require(stringr)
   library(lme4)
-  require(dplyr) # current version uses dplyr piping
   library(dplyr)
+  library(stringr)
   lmerd.output = list()
   ba = max(contrast) - min(contrast) # For transformations needed to make calculations insensitive to contrast weights
   c2 = (ba/2)^2 # For transforming random slope variance
   contr.est = lme4::fixef(model) * ba # Extracting fixed effects, transformed for contrast weights
-  contr.SE = summary(model)$coefficients %>% as.data.frame() %>% pull(`Std. Error`) * ba
+  contr.SE = dplyr::pull(as.data.frame(summary(model)$coefficients), `Std. Error`) * ba
   contr.CI.low = contr.est - (1.96 * contr.SE)
   contr.CI.high = contr.est + (1.96 * contr.SE)
   unweighted.variances = list()
   variance.list = list()
   for (j in c(1:length(lme4::VarCorr(model)))) {
-    variance.list[[j]] = lme4::VarCorr(model)[[j]] %>% as.matrix() %>% diag() #to extract variances and not covariances
+    variance.list[[j]] = diag(as.matrix(lme4::VarCorr(model)[[j]])) #to extract variances and not covariances
     unweighted.variances[[j]] = variance.list[[j]]
-    variance.list[[j]][names(variance.list[[j]]) %>% str_detect("Intercept") == F] = 
-      variance.list[[j]][names(variance.list[[j]]) %>% str_detect("Intercept") == F] * c2
+    variance.list[[j]][stringr::str_detect(names(variance.list[[j]]), "Intercept") == F] = 
+      variance.list[[j]][stringr::str_detect(names(variance.list[[j]]), "Intercept") == F] * c2 #adjusting random slopes for contrast weights
   }
   variance.list[[(length(lme4::VarCorr(model)) + 1)]] = sigma(model)^2 # to add residual variance
   unweighted.variances[[(length(lme4::VarCorr(model)) + 1)]] = sigma(model)^2 # to add residual variance
-  lmerd.output$sd.pooled = variance.list %>% unlist %>% sum() %>% sqrt()
+  lmerd.output$sd.pooled = sqrt(sum(unlist(variance.list)))
   lmerd.output$d.est = contr.est / lmerd.output$sd.pooled
   lmerd.output$d.CI.low = contr.CI.low / lmerd.output$sd.pooled
   lmerd.output$d.CI.high = contr.CI.high / lmerd.output$sd.pooled
-  lmerd.output$weighted.variances = variance.list %>% unlist()
-  lmerd.output$unweighted.variances = unweighted.variances %>% unlist()
+  lmerd.output$weighted.variances =  unlist(variance.list)
+  lmerd.output$unweighted.variances = unlist(unweighted.variances)
   if (formatted == T) {
     formatted.lmerd.output = data.frame(Terms = names(lmerd.output$d.est), 
                                         d_95ci = paste0(format(round(lmerd.output$d.est, 2), nsmall = 2, trim = T), 
